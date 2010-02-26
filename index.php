4b782ae1515fd3b8168918709d6ddff61d2e6313
tarendt <tarendt>
2010-02-26 08:48:25 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -110,6 +110,8 @@ models, e.g. UML EMF models.
 	
 		
    		<h6>Incubation</h6>
+		<p>This component is currently in its <a href="http://www.eclipse.org/projects/dev_process/validation-phase.php">Validation (Incubation) Phase</a>.</p>
+
    			<div align="center"><a href="/projects/what-is-incubation.php"><img align="center" src="/images/egg-incubation.png"  border="0" alt="Incubation" /></a></div>
  			</div>
 	<!--	

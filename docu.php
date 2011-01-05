616501a3405b6fcad48f225dc146de29c4be350d
tarendt <tarendt>
2011-01-05 08:51:00 +0000

    *** empty log message ***

--- docu.php
+++ docu.php
@@ -1,33 +1,9 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
-	#*****************************************************************************
-	#
-	# template.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
-	
-	#
-	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "EMF Refactor - Documentation";
 	$pageKeywords	= "EMF, Refactor, model refactoring, refactoring";
 	$pageAuthor		= "Thorsten Arendt";
 
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-
-	# End: page-specific settings
-	#
-		
-	# Paste your HTML content between the EOHTML markers!	
 	$html = <<<EOHTML
 
 <div id="maincontent">
@@ -38,12 +14,8 @@
 		<h2>How to's</h2>
 		<ul class="midlist">
 			<li><a href="application.php">How to apply EMF model refactorings</a></li>
-			<li><a href="#generate">How to generate new EMF model refactorings</a></li>
+			<li><a href="generation.php">How to generate new EMF model refactorings using Java</a></li>
 		</ul>
-		<h2><a name="application.php">How to apply EMF model refactorings</a></h2>
-		<i>More information here soon...</i>
-		<h2><a name="generate">How to generate new EMF model refactorings</a></h2>
-		<i>More information here soon...</i>
 		</p>
 		
 	</div>
--- generation.php
+++ generation.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
-	$pageTitle 		= "How to apply EMF model refactorings";
+	$pageTitle 		= "How to generate new EMF model refactorings using Java";
 	$pageKeywords	= "EMF, Refactor, model refactoring, refactoring";
 	$pageAuthor		= "Thorsten Arendt";
 

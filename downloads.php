a5a793730f3869bbfdfc410bc937a10896ca502e
tarendt <tarendt>
2012-06-23 20:59:08 +0000

    *** empty log message ***

--- downloads.php
+++ downloads.php
@@ -39,7 +39,9 @@
 			<li><a href="downloads/ExampleProjectRSA755.zip" target="_blank">Example project for UML modeling environment IBM Rational Software Architect v7.5.5</a></li>
 		</ul>
 		<b>Papyrus:</b> Please install the Eclipse Modeling Tools from <a href="http://www.eclipse.org/downloads" target="_blank">www.eclipse.org/downloads</a></li>.
-		Start Eclipse and install the UML modeling environment Papyrus (<i>Install Modeling Components > Papyrus</i>). Download <i>Papyrus.zip</i> from above, unzip it
+		Start Eclipse and install the UML modeling environment Papyrus (<i>Install Modeling Components > Papyrus</i>) as well as the reporting tool BIRT 
+		(<i>Help > Install New Software... > Work with: http://download.eclipse.org/birt/update-site/3.7-interim/ > Select All > Next > ...). 
+		Download <i>Papyrus.zip</i> from above, unzip it
 		into the plugins directory of your Eclipse installation and restart Eclipse. Download <i>ExampleProjectPapyrus.zip</i> from above and import the example
 		project <i>VehicleRentalCompany</i> into your workspace.
 		<br/>

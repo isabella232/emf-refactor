b944ef7d1b64c44ce10dd42c6641fa33f297ebe3
tarendt <tarendt>
2013-01-07 15:32:49 +0100

    new files added

---
+++ metricsocl.php
@@ -0,0 +1,36 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	$pageTitle 		= "How to generate new EMF model metrics using OCL expressions";
+	$pageKeywords	= "EMF, Refactor, model refactoring, refactoring";
+	$pageAuthor		= "Thorsten Arendt";
+
+	$html = <<<EOHTML
+
+<div id="maincontent">
+
+	<div id="midcolumn">
+		<a name=top></a>
+		<h1>$pageTitle</h1>		
+		... coming soon ...	
+	</div>
+	
+	<div id="rightcolumn">
+		<div class="sideitem">	
+		
+   		<h6>Incubation</h6>
+		<p>This component is currently in its <a href="http://www.eclipse.org/projects/dev_process/validation-phase.php">Validation (Incubation) Phase</a>.</p>
+
+   			<div align="center"><a href="/projects/what-is-incubation.php"><img align="center" src="/images/egg-incubation.png"  border="0" alt="Incubation" /></a></div>
+ 		</div>
+		
+	</div>
+		
+</div>
+
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
